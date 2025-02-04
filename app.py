41275b8c81df259b19fb61237b0d2411fea3ae6c
Anayat Ali <anayatalifw@gmail.com>
2025-02-04 01:46:38 -0500

    updated code

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,9 +6,12 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/api/data')
+@app.route('/api/data', methods=['POST'])
 def get_data():
-    return {'data': [1, 2, 3]}
+    if request.method == 'POST':
+        data = request.get_json()
+        print(data)
+        return data
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
