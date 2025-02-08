62b9d667c8f72950086d1a2ddf49d8f496166618
Inayat Ali <anayatalifw@gmail.com>
2025-02-08 16:52:05 -0500

    added prime function

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import math
 
 app = Flask(__name__)
 
@@ -10,8 +11,26 @@ def home():
 def get_data():
     if request.method == 'POST':
         data = request.get_json()
-        print(data)
-        return data
+        value = int(data['value'])
+        result = None
+        
+        # Edge cases
+        if value == 1:
+            result = 'neither'
+        elif value == 2:
+            result = 'prime'
+        elif value % 2 == 0:
+            result = 'composite'
+        else:
+            result = 'prime'
+            max_divisor = int(math.sqrt(value)) + 1
+            for i in range(3, max_divisor, 2):
+                if value % i == 0:
+                    result = 'composite'
+                    break
+                
+        print(value, result)
+        return jsonify({'value': result})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
